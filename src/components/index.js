5b4fc5e87aee06fb2af25fc4504805845d64bb6c
Aladin Exceptionist <sherifmayika@gmail.com>
2024-06-25 17:43:00 +0530

    Delete src/components/index.js

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import Editor from './editor.vue';
-import Loader from './loader.vue';
-import Navbar from './navbar.vue';
-
-Vue.component(Editor.name, Editor);
-Vue.component(Loader.name, Loader);
-Vue.component(Navbar.name, Navbar);
