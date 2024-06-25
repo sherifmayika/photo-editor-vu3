4ef1ddd6a1ff80cb4f24cab3b639a3f39c6f7495
Aladin Exceptionist <sherifmayika@gmail.com>
2024-06-25 17:37:17 +0530

    Create main.js
    vue3 support

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import './styles/index.css';
+//import { library } from '@fortawesome/fontawesome-svg-core';
+//import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+createApp(App).mount('#app');
