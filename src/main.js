c4247582679960b8cd487b65e0a812fd341710f5
Aladin Exceptionist <sherifmayika@gmail.com>
2024-06-26 12:46:32 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './styles/index.css';
+//import './styles/index.css';
 //import { library } from '@fortawesome/fontawesome-svg-core';
 //import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
